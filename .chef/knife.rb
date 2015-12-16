ce821740b6c929a91a953e83ab290d0526bffd5c
Michelle Chua <chuanarra.michelle@gmail.com>
2015-12-16 09:28:15 +0100

    Finish step 1,2 and maybe even 3

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'chua-m'
+client_key               '/Users/fritzyx/Dev/etna/Bachelor_ED/TIC-DAISY/seb/chef-repo/.chef/chua-m.pem'
+validation_client_name   'chef-validator'
+validation_key           '/Users/fritzyx/Dev/etna/Bachelor_ED/TIC-DAISY/seb/chef-repo/.chef/chef-server/chef-validator.pem'
+chef_server_url          'https://michelles-mbp:443'
+syntax_check_cache_path  '/Users/fritzyx/Dev/etna/Bachelor_ED/TIC-DAISY/seb/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ './cookbooks' ]
--- cookbooks/default-route/attributes/default.rb
+++ cookbooks/default-route/attributes/default.rb
@@ -1,2 +1,2 @@
-default['route']['ip_gateway'] = '192.168.110.4'
+default['route']['ip_gateway'] = '192.168.110.5'
 default['route']['device'] = 'eth1'
